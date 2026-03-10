32d0221385e547c86379f71e2800e5c8e911fb12
tiagofreitasdeveloper <tiagofreitas.developer@hotmail.com>
2026-03-10 19:20:00 -0300

    Create UrlMappingRepository.java file

---
+++ src/main/java/com/example/urlshortener/repository/UrlMappingRepository.java
@@ -0,0 +1,12 @@
+package com.example.urlshortener.repository;
+
+import com.example.urlshortener.model.UrlMapping;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UrlMappingRepository extends JpaRepository<UrlMapping, Long> {
+    Optional<UrlMapping> findByShortCode(String shortCode);
+}
